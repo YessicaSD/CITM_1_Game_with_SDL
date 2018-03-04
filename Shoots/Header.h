14103c807eb76674b94f902abc141f0df0a663d1
YessicaSD <36170847+YessicaSD@users.noreply.github.com>
2018-03-04 17:58:41 +0100

    It_has_shoots

--- Shoots/Header.h
+++ Shoots/Header.h
@@ -9,16 +9,21 @@ typedef struct
 }position;
 void render(SDL_Renderer* renderer, position* red)
 {
-	SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);	// Select the color for drawing. It is set to blue here.								
-	SDL_RenderClear(renderer);// Clear the entire screen to our selected color.
-
-	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-	SDL_Rect redRect = { red->x,red->y,50,50 }; // {the x position, the y position, the width,height}
-	SDL_RenderFillRect(renderer, &redRect);
-	SDL_RenderPresent(renderer);
 
 }
-int events(position* red)
+void shoot(SDL_Renderer* renderer, position*green)
+{
+	while ((green->x)<width)
+	{
+		SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+		SDL_Rect redRect = { green->x,green->y,20,20 }; // {the x position, the y position, the width,height}
+		SDL_RenderFillRect(renderer, &redRect);
+		green->x += 1;
+		SDL_RenderPresent(renderer);
+	}
+
+}
+int events(position* red, position*green,SDL_Renderer* renderer)
 {
 	SDL_Event event;
 	while (SDL_PollEvent(&event))
@@ -30,24 +35,41 @@ int events(position* red)
 		}
 		if (event.type == SDL_KEYDOWN)
 		{
-			if (event.key.keysym.sym==SDLK_LEFT && red->x >0)
+			if (event.key.keysym.sym==SDLK_LEFT && red->x >0 )
 			{
-				red->x-=10;
+
+				red->x-=1;
 			}
-			if(event.key.keysym.sym == SDLK_RIGHT && (red->x)<width-50)
+			if(event.key.keysym.sym == SDLK_RIGHT && (red->x)<width-50 )
 			{
 				red->x += 10;
 			}
-			if (event.key.keysym.sym == SDLK_UP && (red->y)>0)
+			if (event.key.keysym.sym == SDLK_UP && (red->y)>0 )
 			{
 				red->y -= 10;
 			}
-			if (event.key.keysym.sym == SDLK_DOWN && (red->y)<height - 50)
+			if (event.key.keysym.sym == SDLK_DOWN && (red->y)<height - 50 )
 			{
 				red->y += 10;
 			}
+			if (event.key.keysym.sym == SDLK_ESCAPE)
+			{
+				return 1;
+			}
+			
+		}
+		if (event.type == SDL_KEYUP)
+		{
+			if (event.key.keysym.sym == SDLK_SPACE)
+			{
+				green->x = red->x;
+				green->y = red->y;
+				shoot(renderer, green);
+			}
 		}
 	}
+	
 }
+
 #endif // !_HEADER_H_
 
--- Shoots/Main.cpp
+++ Shoots/Main.cpp
@@ -1,8 +1,14 @@
 #include "SDL\include\SDL.h"
 #pragma comment (lib,"SDL/x86/SDL2.lib")
 #pragma comment (lib,"SDL/x86/SDL2main.lib")
-#include "Header.h"
 
+int width = 1920;
+int height = 1010;
+typedef struct
+{
+	int x;
+	int y;
+}position;
 int main(int argc, char* argv[])
 {
 	SDL_Init(SDL_INIT_VIDEO);
@@ -13,6 +19,12 @@ int main(int argc, char* argv[])
 	red.x = 960;
 	red.y = 540;
 
+	position green;
+	green.x = red.x;
+	green.y = red.y;
+
+	
+
 	SDL_Window* window; /* Declare a window*/ 
 	SDL_Renderer* renderer; // Declare a render
 
@@ -20,11 +32,80 @@ int main(int argc, char* argv[])
 	window = SDL_CreateWindow("Game", 0,30, width, height, 0);
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED); // SDL_RENDERED_ACCELERATED DOES THE RENDERER WITH THE HARDWARE ACCELERATION
 
-	
+	SDL_Rect redRect = { red.x,red.y,50,50 }; // {the x position, the y position, the width,height}
+	SDL_Rect greenRect = { green.x,green.y,20,20 }; // {the x position, the y position, the width,height}
+
+	SDL_Event event;
 	while (!escape)
 	{
-		render(renderer,&red);
-		escape = events(&red);
+
+		SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);	// Select the color for drawing. It is set to blue here.								
+		SDL_RenderClear(renderer);// Clear the entire screen to our selected color.
+
+		SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+		SDL_RenderFillRect(renderer, &redRect);
+		SDL_RenderPresent(renderer);
+		
+		while (SDL_PollEvent(&event))
+		{
+			if (event.type == SDL_WINDOWEVENT)
+			{
+				if (event.window.event == SDL_WINDOWEVENT_CLOSE) //https://wiki.libsdl.org/SDL_WindowEvent PREGUNTAR PQ ES EVENT.WINDOW.EVENT Y NO EVENT.WINDOW
+					return 1;
+			}
+			if (event.type == SDL_KEYDOWN)
+			{
+				if (event.key.keysym.sym == SDLK_LEFT && redRect.x >0 && event.type != SDL_KEYUP)
+				{
+					redRect.x -= 10;
+				}
+				if (event.key.keysym.sym == SDLK_RIGHT && redRect.x<width - 50 && event.type != SDL_KEYUP)
+				{
+					redRect.x += 10;
+				}
+				if (event.key.keysym.sym == SDLK_UP && (redRect.y)>0 && event.type != SDL_KEYUP)
+				{
+					redRect.y -= 10;
+				}
+				if (event.key.keysym.sym == SDLK_DOWN && (redRect.y)<height - 50 && event.type != SDL_KEYUP)
+				{
+					redRect.y += 10;
+				}
+				if (event.key.keysym.sym == SDLK_ESCAPE && event.type != SDL_KEYUP)
+				{
+					return 1;
+				}
+
+			}
+			if (event.type == SDL_KEYUP)
+			{
+				if (event.key.keysym.sym == SDLK_SPACE)
+				{
+					greenRect.x = redRect.x;
+					greenRect.y = redRect.y;
+					while (greenRect.x <width)
+					{
+						SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);	// Select the color for drawing. It is set to blue here.								
+						SDL_RenderClear(renderer);// Clear the entire screen to our selected color.
+
+						SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+						SDL_RenderFillRect(renderer, &greenRect);
+						greenRect.x += 1;
+
+						SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+						SDL_RenderFillRect(renderer, &redRect);
+						
+
+						
+
+						SDL_RenderPresent(renderer);
+						
+					}
+				}
+			}
+		}
+
+		
 	}
 	SDL_DestroyWindow(window);
 	SDL_DestroyRenderer(renderer);
@@ -32,3 +113,4 @@ int main(int argc, char* argv[])
 	
 	return 0;
 }
+
